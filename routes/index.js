45520900cbb7cd444a9fca9514eef5225b93a202
cponeill <caseyoneill78@hotmail.com>
2014-07-28 10:07:58 -0700

    minor change

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@
 exports.index = function(req, res){
   res.render('index', {
   	title: 'Blockbit',
-  	find: 'The easiest way to find the amount of any Bitcoin on the Blockchain!'
+  	find: 'Find the amount of any Bitcoin on the Blockchain!'
   });
 };
